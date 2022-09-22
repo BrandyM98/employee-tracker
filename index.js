ed13e28fad68b2a4aaa2e294146a3ff170c856a4
Brandy Moore <brandymoore923@gmail.com>
2022-09-21 19:24:13 -0500

    added choices to prompt and added switch cases

--- index.js
+++ index.js
@@ -5,14 +5,26 @@ const db = require('./db');
 
 //inquirer prompt
 
-function init () {
+function init() {
     inquirer.prompt([
         {
             type: 'list',
-            message: 'question?',
+            message: 'What would you like to view?',
             name: 'options',
             choices: [
-
+                'View all departments',
+                'View all roles',
+                'View all employees',
+                'Add a department',
+                'Add an employee',
+                'Update employee role',
+                'Update employee managers',
+                'View employees by manager',
+                'View employees by department',
+                'Delete a department',
+                'Delete a role',
+                'Delete an employee',
+                'Exit',
             ],
         }
     ]).then((response) => {
@@ -20,15 +32,51 @@ function init () {
             case 'View departments':
                 viewAllDepartments()
                 break;
+            case 'View all roles':
+                viewAllRoles()
+                break;
+            case 'View all employees':
+                viewAllEmployees()
+                break;
+            case 'Add a department':
+                addADepartment()
+                break;
+            case 'Add an employee':
+                addAnEmployee()
+                break;
+            case 'Update employee role':
+                updateEmployeeRole()
+                break;
+            case 'Update employee managers':
+                updateEmployeeManagers()
+                break;
+            case 'View employees by manager':
+                viewEmployeesByManager()
+                break;
+            case 'View employees by department':
+                viewEmployeesByDepartment()
+                break;
+            case 'Delete a department':
+                deleteADepartment()
+                break;
+            case 'Delete a role':
+                deleteARole()
+                break;
+            case 'Delete an employee':
+                deleteAnEmployee()
+                break;
+            case 'Exit':
+                exit()
+                break;
         }
     })
 }
 
 //view all departments function
-function viewAllDepartments (){
+function viewAllDepartments() {
     db.viewDepartments()
-    .then(([departments])=> {
-        console.table(departments) 
-        init ()
-    })
+        .then(([departments]) => {
+            console.table(departments)
+            init()
+        })
 }
